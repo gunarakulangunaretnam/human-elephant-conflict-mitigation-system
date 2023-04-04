22b59b3f6b11f7aeb9463e9e442afc423c1d3a41
GunaRakulan-GR <gunarakulan@gmail.com>
2023-04-04 16:36:05 +0530

    feat(mos): add video frame to the tkinter window

--- source/elepment-monitoring-system-main.py
+++ source/elepment-monitoring-system-main.py
@@ -1,7 +1,9 @@
 import cv2
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 from PIL import Image, ImageTk
+import PIL.Image, PIL.ImageTk
 
 class App:
 
@@ -10,30 +12,69 @@ class App:
         filename = filedialog.askopenfilename(title="Select a File", filetypes=filetypes)
         print(filename)
 
-
     def toggle_textbox(self, target, device_camera_text, ip_camera_text, video_text, browse_button):
         if target == "[DEVICE_CAMERA]":
+
+            device_camera_text.delete(0, "end")
+            ip_camera_text.delete(0, "end")
+            video_text.delete(0, "end")
+
             device_camera_text.configure(state="normal")
             ip_camera_text.configure(state="disabled")
             video_text.configure(state="disabled")
+
             browse_button.configure(state="disabled")
             
 
         elif target == "[IP_CAMERA]":
+
+            device_camera_text.delete(0, "end")
+            ip_camera_text.delete(0, "end")
+            video_text.delete(0, "end")
+
             device_camera_text.configure(state="disabled")
             ip_camera_text.configure(state="normal")
             video_text.configure(state="disabled")
+
             browse_button.configure(state="disabled")
 
         elif target == "[VIDEO]":
+
+            device_camera_text.delete(0, "end")
+            ip_camera_text.delete(0, "end")
+            video_text.delete(0, "end")
+
             device_camera_text.configure(state="disabled")
             ip_camera_text.configure(state="disabled")
             video_text.configure(state="normal")
+
             browse_button.configure(state="normal")
 
-    def __init__(self, window, window_title):
+    def browse_file(self, textbox):
+        # Ask user to select a video file
+        file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4;*.avi;*.mkv")])
+    
+        if file_path:
+            # Set the file path in the entry widget
+            textbox.delete(0, "end")
+            textbox.insert(0, file_path)
+            print(file_path)
+
+    def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title(window_title)
+
+        self.video_source = video_source
+        self.width = 1000
+        self.height = 700
+
+        # create a canvas that can fit the above video source size
+        self.canvas = tk.Canvas(window, width=self.width, height=self.height)
+        self.canvas.place(x=30, y=30)
+
+        # set up the main loop
+        self.delay = 15 # milliseconds
+        self.update()
 
         # Create a full screen window
         width = window.winfo_screenwidth()
@@ -43,9 +84,9 @@ class App:
         self.window.state("zoomed")
 
         # Setup placeholder image
-        camera_placeholder = tk.PhotoImage(file="assets/camera-placeholder.png")
-        camera_placeholder_label = tk.Label(window, image=camera_placeholder)
-        camera_placeholder_label.place(x=30, y=30)
+        self.camera_placeholder = tk.PhotoImage(file="assets/camera-placeholder.png")
+        self.camera_placeholder_label = tk.Label(window, image=self.camera_placeholder)
+        self.camera_placeholder_label.place(x=30, y=30)
 
 
         # Create a button for Snapshot
@@ -84,7 +125,7 @@ class App:
         file_path_text.place(x=250, y=165)
 
         # Create a browse button for Pre-Recorded Videos
-        browse_button = tk.Button(options_frame, text="Browse", font=("Arial", 12, "bold"), state="disabled")
+        browse_button = tk.Button(options_frame, text="Browse", font=("Arial", 12, "bold"), state="disabled", command=lambda: self.browse_file(file_path_text))
         browse_button.place(x=567, y=156)
 
         # Create a horizontal separator
@@ -125,7 +166,7 @@ class App:
         self.contrast_slider.place(x=250, y=520)
 
         # Create a browse button for Pre-Recorded Videos
-        start_processing_nutton = tk.Button(options_frame, text="Start Processing", font=("Arial", 12, "bold"))
+        start_processing_nutton = tk.Button(options_frame, text="Start Processing", font=("Arial", 12, "bold"), command=self.start_processing)
         start_processing_nutton.configure(bg="dark green", fg="white")
         start_processing_nutton.place(x=576, y=650)
 
@@ -137,6 +178,37 @@ class App:
         # Start the GUI loop
         self.window.mainloop()
 
+    def start_processing(self):
+        # open video source (by default this will try to open the computer webcam)
+        self.camera_placeholder_label.place_forget()
+        self.vid = cv2.VideoCapture(self.video_source)
+        self.window.update()
+
+    def update(self):
+        if hasattr(self, 'vid'):
+            # Get a frame from the video source
+            ret, frame = self.vid.read()
+
+            if ret:
+                # Convert the frame to RGB format and resize it to fit the canvas
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, (self.width, self.height))
+
+                # Convert the frame to an ImageTk object and display it on the canvas
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+                self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+
+            # Calculate and display the frame rate
+            self.fps_label.config(text="FPS: {:.2f}".format(self.vid.get(cv2.CAP_PROP_FPS)))
+
+        # Schedule the next update after a delay
+        self.window.after(self.delay, self.update)
+        
+
+    def __del__(self):
+        # Release the video source when the object is destroyed
+        if hasattr(self, 'vid') and self.vid.isOpened():
+            self.vid.release()
 
     
 # Create the application window
