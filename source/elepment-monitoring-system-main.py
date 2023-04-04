16baf5a6536b32f8fe27edb82a15d620145d950f
GunaRakulan-GR <gunarakulan@gmail.com>
2023-04-04 18:00:57 +0530

    feat(mos): add elepment detection model with the UI app

--- source/elepment-monitoring-system-main.py
+++ source/elepment-monitoring-system-main.py
@@ -4,63 +4,60 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import PIL.Image, PIL.ImageTk
+import os                                                                  # To Perform OS level works.
+import six
+import cv2                                                                 # OpenCV for Computer Vision                                                        # It has a dictionary that contains colors for each label
+import argparse                                                            # To get arguments
+import collections
+import numpy as np
+import pyttsx3                                                             # To perform text to speech function
+import threading                                                           # To perform multi-threading operations
+import playsound                                                           # To play sounds
+import tensorflow as tf                                                    # Main Library.
+from object_detection.utils import label_map_util                          # To handle label map.
+from object_detection.utils import config_util                             # To load model pipeline.
+from object_detection.utils import visualization_utils as viz_utils        # To draw rectangles.
+from object_detection.builders import model_builder                        # To load & Build models.
+
+
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+model_config_path =  f'data/models/ssd_mobilenet_v2_320x320_coco17_tpu-8/pipeline.config'        # Store the path of config file
+checkpoint_model_path   =  f'data/models/ssd_mobilenet_v2_320x320_coco17_tpu-8/checkpoint/ckpt-0'      # Store the path of model
+label_map_path    =  f'data/mscoco_label_map.pbtxt'                             # Store the path of label_map
+
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(model_config_path)
+model_config = configs['model']
+detection_model = model_builder.build(model_config=model_config, is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(checkpoint_model_path).expect_partial()
+
+@tf.function
+
+def detect_fn(image):
+    """Detect objects in image."""
+
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+
+    return detections, prediction_dict, tf.reshape(shapes, [-1])
+
+
+category_index = label_map_util.create_category_index_from_labelmap(label_map_path,use_display_name=True)
+
 
 class App:
 
-    def browse_file(self):
-        filetypes = (("Video Files", "*.mp4 *.avi"), ("All Files", "*.*"))
-        filename = filedialog.askopenfilename(title="Select a File", filetypes=filetypes)
-        print(filename)
-
-    def toggle_textbox(self, target):
-        if target == "[DEVICE_CAMERA]":
-
-            self.device_camera_text.delete(0, "end")
-            self.ip_camera_text.delete(0, "end")
-            self.file_path_text.delete(0, "end")
-
-            self.device_camera_text.configure(state="normal")
-            self.ip_camera_text.configure(state="disabled")
-            self.file_path_text.configure(state="disabled")
-
-            self.browse_button.configure(state="disabled")
-            
-
-        elif target == "[IP_CAMERA]":
-
-            self.device_camera_text.delete(0, "end")
-            self.ip_camera_text.delete(0, "end")
-            self.file_path_text.delete(0, "end")
-
-            self.device_camera_text.configure(state="disabled")
-            self.ip_camera_text.configure(state="normal")
-            self.file_path_text.configure(state="disabled")
-
-            self.browse_button.configure(state="disabled")
-
-        elif target == "[VIDEO]":
-
-            self.device_camera_text.delete(0, "end")
-            self.ip_camera_text.delete(0, "end")
-            self.file_path_text.delete(0, "end")
-
-            self.device_camera_text.configure(state="disabled")
-            self.ip_camera_text.configure(state="disabled")
-            self.file_path_text.configure(state="normal")
-
-            self.browse_button.configure(state="normal")
-
-    def browse_file(self):
-        # Ask user to select a video file
-        file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4;*.avi;*.mkv")])
-    
-        if file_path:
-            # Set the file path in the entry widget
-            self.file_path_text.delete(0, "end")
-            self.file_path_text.insert(0, file_path)
-            print(file_path)
-
-    def __init__(self, window, window_title, video_source=0):
+    def __init__(self, window, window_title, video_source='test-video.m4v'):
         self.window = window
         self.window.title(window_title)
 
@@ -185,23 +182,137 @@ class App:
         self.vid = cv2.VideoCapture(self.video_source)
         self.window.update()
 
+    def detect_fn(image):
+        """Detect objects in image."""
+
+        image, shapes = detection_model.preprocess(image)
+        prediction_dict = detection_model.predict(image, shapes)
+        detections = detection_model.postprocess(prediction_dict, shapes)
+
+        return detections, prediction_dict, tf.reshape(shapes, [-1])
+
     def update(self):
         if hasattr(self, 'vid'):
             # Get a frame from the video source
             ret, frame = self.vid.read()
 
             if ret:
-                # Convert the frame to RGB format and resize it to fit the canvas
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                frame = cv2.resize(frame, (self.width, self.height))
+                image_np_expanded = np.expand_dims(frame, axis=0)
+                input_tensor = tf.convert_to_tensor(np.expand_dims(frame, 0), dtype=tf.float32)
+                detections, predictions_dict, shapes = detect_fn(input_tensor)
 
-                # Convert the frame to an ImageTk object and display it on the canvas
-                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+                label_id_offset = 1
+                image_np_with_detections = frame.copy()
+
+                min_score_thresh = 0.50
+
+                box_to_display_str_map = collections.defaultdict(list)
+                box_to_color_map = collections.defaultdict(str)
+
+                number_of_items = 0
+
+                for i in range(detections['detection_boxes'][0].numpy().shape[0]):
+
+                    if detections['detection_scores'][0].numpy() is None or detections['detection_scores'][0].numpy()[i] > min_score_thresh:
+
+                        box = tuple(detections['detection_boxes'][0].numpy()[i].tolist())
+
+                        display_str = ""
+
+                        if(detections['detection_classes'][0].numpy() + label_id_offset).astype(int)[i] in six.viewkeys(category_index):
+                            class_name = category_index[(detections['detection_classes'][0].numpy() + label_id_offset).astype(int)[i]]['name']
+                            display_str = '{}'.format(class_name)
+
+                            box_to_display_str_map[box].append(display_str) # Join the number of eleements with label Name
+
+
+                im_width, im_height = frame.shape[1::-1]
+
+                for box, color in box_to_display_str_map.items():
+                    ymin, xmin, ymax, xmax = box
+
+                    ymin = ymin * im_height
+                    xmin = xmin * im_width
+                    ymax = ymax * im_height
+                    xmax = xmax * im_width
+
+                    x = xmin
+                    y = ymin
+                    w = xmax - xmin
+                    h = ymax - ymin
+
+                    if box_to_display_str_map[box][0].replace("_"," ") == "elephant": # Get only label name not the total number of items
+
+                        cv2.rectangle(image_np_with_detections, (int(x),int(y)), (int(x) + int(w), int(y) + int(h)), (0, 0, 255), 4)
+                        (tw, th), _ = cv2.getTextSize(box_to_display_str_map[box][0], cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+
+                        # Prints the text.
+                        img = cv2.rectangle(image_np_with_detections, (int(x), int(y) - 30), (int(x) + 20 + tw, int(y)), (0, 0, 255), -1)
+                        img = cv2.putText(image_np_with_detections, box_to_display_str_map[box][0].upper(), (int(x)+5, int(y) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 2)
+
+                final_frame = cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB)
+                final_frame = cv2.resize(final_frame, (self.width, self.height))
+
+                self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(final_frame))
                 self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
+                           
         # Schedule the next update after a delay
         self.window.after(self.delay, self.update)
         
+
+    def browse_file(self):
+        filetypes = (("Video Files", "*.mp4 *.avi"), ("All Files", "*.*"))
+        filename = filedialog.askopenfilename(title="Select a File", filetypes=filetypes)
+        print(filename)
+
+    def toggle_textbox(self, target):
+        if target == "[DEVICE_CAMERA]":
+
+            self.device_camera_text.delete(0, "end")
+            self.ip_camera_text.delete(0, "end")
+            self.file_path_text.delete(0, "end")
+
+            self.device_camera_text.configure(state="normal")
+            self.ip_camera_text.configure(state="disabled")
+            self.file_path_text.configure(state="disabled")
+
+            self.browse_button.configure(state="disabled")
+            
+
+        elif target == "[IP_CAMERA]":
+
+            self.device_camera_text.delete(0, "end")
+            self.ip_camera_text.delete(0, "end")
+            self.file_path_text.delete(0, "end")
+
+            self.device_camera_text.configure(state="disabled")
+            self.ip_camera_text.configure(state="normal")
+            self.file_path_text.configure(state="disabled")
+
+            self.browse_button.configure(state="disabled")
+
+        elif target == "[VIDEO]":
+
+            self.device_camera_text.delete(0, "end")
+            self.ip_camera_text.delete(0, "end")
+            self.file_path_text.delete(0, "end")
+
+            self.device_camera_text.configure(state="disabled")
+            self.ip_camera_text.configure(state="disabled")
+            self.file_path_text.configure(state="normal")
+
+            self.browse_button.configure(state="normal")
+
+    def browse_file(self):
+        # Ask user to select a video file
+        file_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4;*.avi;*.mkv")])
+    
+        if file_path:
+            # Set the file path in the entry widget
+            self.file_path_text.delete(0, "end")
+            self.file_path_text.insert(0, file_path)
+            print(file_path)
 
     def __del__(self):
         # Release the video source when the object is destroyed
